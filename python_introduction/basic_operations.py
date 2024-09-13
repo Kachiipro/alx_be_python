2481f23db098ba2bfed72e1cd7dd40cb3050f8e1
Dikachi J <dikachimichael253@gmail.com>
2024-09-13 08:13:00 +0100

    wrote a script that performs basic operation withtwo predefined numbers

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+# arithmetic operations with two predefined numbers
+
+number1 = 50
+number2 = 5
+
+sum = number1 + number2
+
+difference = number1 - number2
+
+product = number1 * number2
+
+# print result of operations
+
+print(f"Addition of {number1} and {number2} is ", sum)
+print(f"Subtraction of {number1} and {number2} is", difference)
+print(f"Multiplication of {number1} and {number2} is", product)
