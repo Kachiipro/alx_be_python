e6a8badbd0ba9672c38989310496a60fc345d6ce
Dikachi J <dikachimichael253@gmail.com>
2024-09-15 00:17:57 +0100

    wrote script to project annual income

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,10 +1,11 @@
 # Use user input, variables, and arithmetic operations to calculate and provide feedback on a user’s monthly savings and potential future savings without applying conditional statements.
 
 monthly_income = int(input("Enter your monthly income:"))
-total_expenses = int(input("Enter your total monthly expenses:"))
+monthly_expenses = int(input("Enter your total monthly expenses:"))
 
 monthly_savings = monthly_income - total_expenses
-annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 print(f"Your monthly savings are ${monthly_savings}")
-print(f"Projected savings after one year, with interest, is: ${annual_savings}")
+print(f"Projected savings after one year, with interest, is: ${
+      annual_savings}")
