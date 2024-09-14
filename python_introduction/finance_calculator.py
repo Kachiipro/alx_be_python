e8cbb5b31cd6385570292d67fbe8d611b522bbb6
Dikachi J <dikachimichael253@gmail.com>
2024-09-14 23:39:50 +0100

    created finance_calculator.py file

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,1 @@
+# Use user input, variables, and arithmetic operations to calculate and provide feedback on a user’s monthly savings and potential future savings without applying conditional statements.
