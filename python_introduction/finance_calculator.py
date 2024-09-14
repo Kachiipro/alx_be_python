17d2f38820e75220d0a1219d9b27a79f73590a80
Dikachi J <dikachimichael253@gmail.com>
2024-09-15 00:21:42 +0100

    corrected missing variable on finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,9 +3,8 @@
 monthly_income = int(input("Enter your monthly income:"))
 monthly_expenses = int(input("Enter your total monthly expenses:"))
 
-monthly_savings = monthly_income - total_expenses
+monthly_savings = monthly_income - monthly_expenses
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 print(f"Your monthly savings are ${monthly_savings}")
-print(f"Projected savings after one year, with interest, is: ${
-      annual_savings}")
+print(f"Projected savings after one year, with interest, is: ${annual_savings}")
