8b0b0872fe4d0e55290aee3b4b94c5ee6a31d326
Dikachi J <dikachimichael253@gmail.com>
2024-09-28 19:32:09 +0100

    created working  arithmetic_operations.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,14 @@
+def perform_operation(num1,num2,operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2   
+    elif operation == "divide" and  num2 == 0:
+            return "Cannot divide by zero."
+    elif operation == "divide":
+        return num1 / num2  
+    else:
+         print("Invalid operation. Please choose (add, subtract, multiply, divide):")
+
