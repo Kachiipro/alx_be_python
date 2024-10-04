42289ccf35f01682e6a0ec929f47ca3dc943bdbe
Dikachi J <dikachimichael253@gmail.com>
2024-10-04 04:00:46 +0100

    created a bank account script

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self, account):
+        self.account = account
+        self.account_balance = 0
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if self.account_balance < amount:
+            return False
+        else:
+            self.account_balance -= amount
+            return True
+
+    def display_balance(self):
+        print(f"Your available balance is {self.account_balance}")
